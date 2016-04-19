318a66e767312acb8a9f448ecb412acf48c6bb1d
yusuke-can <a.plague.of.locusts@gmail.com>
2016-04-20 06:32:44 +0900

    maven-protof-plugin でProtocolBuffersクラスを生成

---
+++ src/test/java/batch/TestProtocolBuffers.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+package batch;
+
+
+
+/**
+ * @author Yusuke Kyan
+ *
+ */
+public class TestProtocolBuffers {
+// final ProtocolBuffersBook protocolBuffersBook = new ProtocolBuffersBook();
+}
